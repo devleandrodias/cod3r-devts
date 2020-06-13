0b9e9bed82257293a286eccd5bd68856e405957f
devleandrodias <leandrodbdias@gmail.com>
2020-06-13 16:02:01 -0300

    generics with array

--- generics/generics.js
+++ generics/generics.js
@@ -1,15 +1,27 @@
 function echo(object) {
     return object;
 }
-console.log(echo("João"));
+console.log(echo('João'));
 console.log(echo(20));
-console.log(echo({ name: "João", age: 20 }));
+console.log(echo({ name: 'João', age: 20 }));
 function genericEcho(object) {
     return object;
 }
-console.log(genericEcho("Lendro").length);
+console.log(genericEcho('Lendro').length);
 console.log(genericEcho(20).valueOf);
 console.log(genericEcho({
-    name: "Leandro",
+    name: 'Leandro',
     age: 20
 }).name.toUpperCase());
+var cellphones = [112112, 1231231, 123412];
+cellphones.push(1231412);
+console.log(cellphones);
+function genericPrint(args) {
+    args.forEach(function (element) { return console.log(element); });
+}
+genericPrint([10, 12, 14, 234, 123, 1541]);
+genericPrint(['Leandro', 'Thaísa', 'Rhaira', 'João', 'Carlos']);
+genericPrint([
+    { name: 'Leandro', age: 20 },
+    { name: 'Thaísa', age: 20 },
+]);
--- generics/generics.ts
+++ generics/generics.ts
@@ -2,19 +2,37 @@ function echo(object: any) {
   return object;
 }
 
-console.log(echo("João"));
+console.log(echo('João'));
 console.log(echo(20));
-console.log(echo({ name: "João", age: 20 }));
+console.log(echo({ name: 'João', age: 20 }));
 
 function genericEcho<T>(object: T): T {
   return object;
 }
 
-console.log(genericEcho("Lendro").length);
+console.log(genericEcho('Lendro').length);
 console.log(genericEcho<number>(20).valueOf);
 console.log(
   genericEcho<{ name: string; age: number }>({
-    name: "Leandro",
+    name: 'Leandro',
     age: 20,
   }).name.toUpperCase()
 );
+
+const cellphones: Array<number> = [112112, 1231231, 123412];
+
+cellphones.push(1231412);
+
+console.log(cellphones);
+
+function genericPrint<T>(args: T[]): void {
+  args.forEach((element) => console.log(element));
+}
+
+genericPrint<number>([10, 12, 14, 234, 123, 1541]);
+genericPrint<string>(['Leandro', 'Thaísa', 'Rhaira', 'João', 'Carlos']);
+
+genericPrint<{ name: string; age: number }>([
+  { name: 'Leandro', age: 20 },
+  { name: 'Thaísa', age: 20 },
+]);
